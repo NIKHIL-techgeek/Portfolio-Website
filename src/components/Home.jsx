94e7e8b066dab1e22520e50510e9dd76981568bc
Nikhil Kapoor <nikhilkapoor.in@gmail.com>
2022-07-27 13:39:31 -0400

    Initializing Home Page

---
+++ src/components/Home.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi';
+const Home = () => {
+    
+}
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {FaBars , FaTimes} from 'react-icons/fa'
 import logo from '../assets/logo.svg'
 function Navbar() {
   const [nav , setNav] = useState(false);
-  const handleClick = () => {}
+  const handleClick = () => setNav(!nav);
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -20,12 +20,12 @@ function Navbar() {
 
       
           {/* Hamburger */}
-          <div className='md:hidden z-10'>
-            <FaBars/>
+          <div  onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars/> : <FaTimes/>}
           </div>
 
           {/* Mobile menu */}
-            <ul className=' hidden absolute top-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+            <ul className={!nav ? 'hidden' : 'absolute top-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
               <li className='py-6 text-4xl'>Home</li>
               <li className='py-6 text-4xl'>About</li>
               <li className='py-6 text-4xl'>Skills</li>
