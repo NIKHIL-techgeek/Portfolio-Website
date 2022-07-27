0742ab3af6f7eef4d158f330f8d2fbdd3a822b5d
Nikhil Kapoor <nikhilkapoor.in@gmail.com>
2022-07-27 13:21:46 -0400

    Working to add state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import {FaBars , FaTimes} from 'react-icons/fa'
 import logo from '../assets/logo.svg'
 function Navbar() {
+  const [nav , setNav] = useState(false);
+  const handleClick = () => {}
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
           <img src={logo} alt='logo' style={{width:"60px"}}/>
       </div>
       {/* menu */}
-      
             <ul className='hidden md:flex'>
               <li>Home</li>
               <li>About</li>
@@ -16,20 +17,21 @@ function Navbar() {
               <li>Work</li>
               <li>Contact</li>
             </ul>
-        
+
+      
           {/* Hamburger */}
-          <div className='md:hidden'>
+          <div className='md:hidden z-10'>
             <FaBars/>
           </div>
-          {/* Mobile menu */}
-            <ul className='absolute top-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
-            <li>Home</li>
-              <li>About</li>
-              <li>Skills</li>
-              <li>Work</li>
-              <li>Contact</li>
-            </ul>
 
+          {/* Mobile menu */}
+            <ul className=' hidden absolute top-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+              <li className='py-6 text-4xl'>Home</li>
+              <li className='py-6 text-4xl'>About</li>
+              <li className='py-6 text-4xl'>Skills</li>
+              <li className='py-6 text-4xl'>Work</li>
+              <li className='py-6 text-4xl'>Contact</li>
+            </ul>
           {/* Social Icons */}
           <div className='hidden'></div>
     </div>
