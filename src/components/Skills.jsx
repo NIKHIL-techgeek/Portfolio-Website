6122094348e243d588f8fa41375099303314c637
Nikhil Kapoor <nikhilkapoor.in@gmail.com>
2022-07-30 12:57:40 -0400

    Done with skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,12 +11,12 @@ import github from '../assets/github.png'
 import python from '../assets/python.png'
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='bg-[#0a192f] text-gray-300 w-full h-screen'>
         {/* container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>
-                    Experience
+                    Skills
                 </p>
                 <p className='py-4'>
                     The Technologies I had worked with
