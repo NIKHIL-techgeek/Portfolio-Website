c14c9560055feccafafa115bfbacef649ad2d382
Nikhil Kapoor <nikhilkapoor.in@gmail.com>
2022-08-07 14:32:44 -0400

    remove hover from navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,19 +9,20 @@ const Home = () => {
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
       <p className='text-[#f41a85]'>Hey , My name is</p>
       <h1 className='text-4xl sm:text-7xl font-bold text-[#d4cacf]'>Nikhil Kapoor</h1>
-      <h2 className='text-4xl sm:text-7xl font-bold text-[#908a8d]'>I am Full Time International Student.</h2>
-      <p className='text-[#908a8d] py-4 max-w-[700px]'>I love working on new Technologies and make projects using it. 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Voluptatibus tenetur cum porro ab itaque ipsam animi consequatur id voluptatem modi officia iusto, 
-        facere, debitis repellendus ad tempora quibusdam accusantium accusamus harum reprehenderit commodi</p>
+      <h2 className='text-4xl sm:text-7xl font-bold text-[#908a8d]'>I am Full Time International Student at University of Windsor .</h2>
+      <p className='text-[#908a8d] py-4 max-w-[700px]'>I love working on new Technologies and making projects with it.</p>
+      <p className='text-[#908a8d] py-2 max-w-[700px]'>Go To Skills Section to see Technologies that I had worked with!</p>
         <div>
-          <button className='text-[#ffffff] group border-2 px-6 py-3 my-2 flex items-center
+        <Link to="skills"  smooth={true} duration={500} >
+        <button className='text-[#ffffff] group border-2 px-6 py-3 my-2 flex items-center
            hover:bg-[#f41a85] hover:border-[#f41a85]'>
-          View Work
+          Skills
           <span className='group-hover:rotate-90 duration-300'>
           <HiArrowNarrowRight className='ml-2.5'/>
           </span>
           </button>
+              </Link>
+         
         </div>
       </div>
 
